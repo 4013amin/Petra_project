b9d216f14d1d46a634f072e8617fdfd27b71da0c
delta <amin138400138400@gmail.com>
2025-04-29 13:51:49 +0330

    Edit and push in server liara

--- app/src/main/java/com/example/shop_app_project/Home_page/Main/Main_page.kt
+++ app/src/main/java/com/example/shop_app_project/Home_page/Main/Main_page.kt
@@ -373,7 +373,7 @@ fun ProductItem(
             // Product Image
             if (images.isNotEmpty()) {
                 AsyncImage(
-                    model = "https://petshopdjango.liara.run${images.first()}",
+                    model = "https://petshopdjango.liara.run/${images.first()}",
                     contentDescription = null,
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
